4cfa5ef3c53bd2c82b5ecf7d406013ba33f6c453
Bagaric <joseph@josephit.com>
2018-01-26 10:43:23 +0100

    Added truffle develop to travis build workflow

--- truffle.js
+++ truffle.js
@@ -5,6 +5,12 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       gas: 4700000
+    },
+    truffle_develop: {
+      host: "localhost",
+      port: 9545,
+      network_id: "*", // Match any network id
+      gas: 4700000
     }
   }
 };
